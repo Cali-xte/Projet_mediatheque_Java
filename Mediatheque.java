55b46f2d4c74260a726d5ded8f72da4671b2f305
Calixte <41114634+Cali-xte@users.noreply.github.com>
2021-04-19 12:19:50 +0200

    Création/correction des menus d'ajout d'œuvre et de gestion de la médiathèque
    Déplacement des inputs d'ajout d'œuvre vers la classe Mediatheque (méthode ajOeuvre(), création des différents menus.

--- Mediatheque.java
+++ Mediatheque.java
@@ -6,12 +6,67 @@ public class Mediatheque {
     ArrayList<Oeuvre> mediatheque = new ArrayList<Oeuvre>();
 
     // Méthodes
-    public void ajOeuvre(Oeuvre oeuvre) {
-        mediatheque.add(oeuvre);
+    public void ajOeuvre() {
+        Scanner nouveau = new Scanner(System.in);
+        // Menu d'ajout, choix du type
+        System.out.println("Vous souhaitez ajouter :");
+        System.out.println("1- Une oeuvre sans type specifique");
+        System.out.println("2- Un livre");
+        System.out.println("3- Une video");
+        System.out.println("4- Une musique");
+        String type = nouveau.nextLine();
+        // Infos communes à tous les types d'oeuvres
+        System.out.println("Entrez les informations demandées");
+        System.out.println("Reference :");
+        String referencein = nouveau.nextLine();
+        System.out.println("Auteur :");
+        String auteurin = nouveau.nextLine();
+        System.out.println("Titre :");
+        String titrein = nouveau.nextLine();
+        System.out.println("Annee :");
+        String anneein = nouveau.nextLine();
+
+        if (type.equals("1")) {
+            Oeuvre nouvoeuvre = new Oeuvre(referencein, auteurin, titrein, anneein);
+            mediatheque.add(nouvoeuvre);
+        }
+
+        else if (type.equals("2")) {
+            System.out.println("ISBN :");
+            String isbnin = nouveau.nextLine();
+            System.out.println("Editeur :");
+            String editeurin = nouveau.nextLine();
+            System.out.println("Nombre de pages :");
+            String pagesin = nouveau.nextLine();
+            Livre nouvlivre = new Livre(referencein, auteurin, titrein, anneein, isbnin, editeurin, pagesin);
+            mediatheque.add(nouvlivre);
+        }
+
+        else if (type.equals("3")) {
+            System.out.println("Duree :");
+            String dureein = nouveau.nextLine();
+            System.out.println("Format :");
+            String formatin = nouveau.nextLine();
+            Video nouvvideo = new Video(referencein, auteurin, titrein, anneein, dureein, formatin);
+            mediatheque.add(nouvvideo);
+        }
+
+        else if (type.equals("4")) {
+            System.out.println("Nombre de pistes :");
+            String pistesin = nouveau.nextLine();
+            System.out.println("Type de support :");
+            String supportin = nouveau.nextLine();
+            Musique nouvmusique = new Musique(referencein, auteurin, titrein, anneein, pistesin, supportin);
+            mediatheque.add(nouvmusique);
+        }
+
+        else {
+            System.out.println("Erreur, reessayez");
+        }
     }
 
     public void listOeuvre() {
-        System.out.println("----------Liste----------");
+        System.out.println("----------Liste des Oeuvres----------");
         for(int i = 0; i < mediatheque.size(); i++) {
             Oeuvre oeu = mediatheque.get(i);
             oeu.afficher();
--- projet.java
+++ projet.java
@@ -2,61 +2,50 @@ import java.util.Scanner;
 
 public class projet {
     public static void main(String[] args){
-        // Menu principal, choix de l'action
-        Scanner menu = new Scanner(System.in);
-        System.out.println("-------------------------------");
-        System.out.println("---------- Bienvenue ----------");
-        System.out.println("-----Choisissez une option-----");
-        System.out.println("-----en entrant le chiffre-----");
-        System.out.println("---------correspondant---------");
-        System.out.println("-------------------------------");
-        System.out.println("1- Ajouter une oeuvre");
-        System.out.println("2- Lister toutes les oeuvres");
-        System.out.println("3- Rechercher une oeuvre");
-        System.out.println("4- Compter les oeuvres en fonction de leur année de sortie");
-        String choixMenu = menu.nextLine();
+        // Création de la médiathèque
+        Mediatheque media = new Mediatheque();
+        boolean on = true;
+        while (on == true) { //! Boucle de menu (prévoir sortie)
+            // Menu principal, choix de l'action
+            Scanner menu = new Scanner(System.in);
+            System.out.println("-------------------------------");
+            System.out.println("--------Menu Principal---------");
+            System.out.println("-----Choisissez une option-----");
+            System.out.println("-----en entrant le chiffre-----");
+            System.out.println("---------correspondant---------");
+            System.out.println("-------------------------------");
+            System.out.println("1- Ajouter une oeuvre");
+            System.out.println("2- Lister toutes les oeuvres");
+            System.out.println("3- Rechercher une oeuvre");
+            System.out.println("4- Compter les oeuvres en fonction de leur année de sortie");
+            String choixMenu = menu.nextLine();
 
-        if (choixMenu.equals("1")) {
-            // Menu d'ajout, choix du type
-            System.out.println("Vous souhaitez ajouter :");
-            System.out.println("1- Une oeuvre sans type specifie");
-            System.out.println("2- Un livre");
-            System.out.println("3- Une video");
-            System.out.println("4- Une musique");
-            String type = menu.nextLine();
-            // Infos communes à tous les types d'oeuvres
-            System.out.println("Entrez les informations demandées");
-            System.out.println("Reference :");
-            String referencein = menu.nextLine();
-            System.out.println("Auteur :");
-            String auteurin = menu.nextLine();
-            System.out.println("Titre :");
-            String titrein = menu.nextLine();
-            System.out.println("Annee :");
-            String anneein = menu.nextLine();
-            if (type.equals("1")) {
-                //! Utiliser un arraylist pour créer les objets
-                //! https://stackoverflow.com/questions/19961844/creating-multiple-objects-with-different-names-in-a-loop-to-store-in-an-array-li
+            if (choixMenu.equals("1")) {
+                media.ajOeuvre();
+            }
+
+            if (choixMenu.equals("2")) {
+                media.listOeuvre();
+            }
+
+            if (choixMenu.equals("3")) {
+                media.rechOeuvre();
+            }
+
+            if (choixMenu.equals("4")) {
+                System.out.println("Entrez l'annee recherchee :");
+                String choixAnnee = menu.nextLine();
+                int res = media.compteAnnee(choixAnnee);
+                if(res == 0) {
+                    System.out.println("Aucune oeuvre n'est datee de " + choixAnnee);
+                }
+                else if(res == 1) {
+                    System.out.println("1 oeuvre est datee de " + choixAnnee);
+                }
+                else {
+                    System.out.println(res + " oeuvres sont datees de " + choixAnnee);
+                }
             }
         }
-
-        Oeuvre oeuvre = new Oeuvre("112233", "Calixte", "Java", "2021");
-        System.out.println(oeuvre.getAnn());
-        oeuvre.afficher();
-        Livre livre = new Livre("12345", "Etxilac", "Avaj", "1202", "4321", "editions", "1212");
-        livre.afficher();
-        Video video = new Video("refvid", "cal", "Java en vidéos", "2030", "8h30", "avi");
-        video.afficher();
-        Musique musique = new Musique("refmus", "Cali", "la douce musique", "1999", "18", "Cassette");
-        musique.afficher();
-        Mediatheque media = new Mediatheque();
-        media.ajOeuvre(musique);
-        media.ajOeuvre(livre);
-        media.ajOeuvre(oeuvre);
-        media.ajOeuvre(video);
-        media.listOeuvre();
-        media.rechOeuvre();
-        String anneeRecherche = "2021";
-        System.out.println(media.compteAnnee(anneeRecherche) + " oeuvres sont datees de l'annee " + anneeRecherche);
     }
 }
